efd5e4c3bb5c78a578996a118c11f373c1e5731e
nlscng <nlscng.g@gmail.com>
2024-11-20 20:35:41 -0600

    Adding simple hello world, and product except self from a list.

---
+++ src/main/scala/ProductExceptSelf.scala
@@ -0,0 +1,22 @@
+object ProductExceptSelf {
+  private def productExceptSelf(numbers: List[Int]): List[Int] =
+    val numToTake = numbers.size
+    val leftProduct: List[Int] = numbers.scanLeft(1) { (acc, ele) => acc * ele }
+    val leftProductExceptSelf = leftProduct.take(numToTake)
+
+    val rightProduct = numbers.scanRight(1) { (acc, ele) => acc * ele }
+    val rightProductExceptSelf = rightProduct.takeRight(numToTake)
+
+    leftProductExceptSelf.zip(rightProductExceptSelf).map(_ * _)
+
+
+  private def productExceptSelfWithDivision(numbers: List[Int]): List[Int] = {
+    val allProduct = numbers.product
+    numbers.map(allProduct / _)
+  }
+
+  @main def testProductExceptSelf(): Unit =
+    println(productExceptSelf(List(3, 4, 5)))
+    println(productExceptSelfWithDivision(List(3, 4, 5)))
+    println(productExceptSelf(List(3, 4, 5, 6)))
+}
